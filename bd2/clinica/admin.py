bf115eba3baac2b10025629be72f15fa03e7b50b
FireBomb23 <firebomb0623@gmail.com>
2026-05-27 12:18:31 +0100

    new

--- bd2/clinica/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Doente, Contacto, Consulta, Prescricao, Exame, Pagamento
-
-admin.site.register(Doente)
-admin.site.register(Contacto)
-admin.site.register(Consulta)
-admin.site.register(Prescricao)
-admin.site.register(Exame)
-admin.site.register(Pagamento)
